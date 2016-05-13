935c53d147a2e093a08ead66cff080a97c6f5f6f
jthomale <jason.thomale@unt.edu>
2016-05-13 11:50:42 -0500

    Fix name of sudoc normalization test

--- django/sierra/utils/tests/test_helpers.py
+++ django/sierra/utils/tests/test_helpers.py
@@ -10,14 +10,15 @@ LC_NORMALIZATION_TEST_PARAMS = [
     ('AA 1111', 'AA!0000001111'),
     ('AA1111A1111', 'AA!0000001111!A1111'),
     ('AA1111.A1111', 'AA!0000001111!A1111'),
-    #('AA1111 .A1111', 'AA!0000001111!A1111'),
+    ('AA1111 .A1111', 'AA!0000001111!A1111'),
     ('AA1111. A1111', 'AA!0000001111!A1111'),
     ('AA1111.11.A1111 A1111 2000', 'AA!0000001111.11!A1111!A1111!0000002000'),
 ]
 
 SUDOC_NORMALIZATION_TEST_PARAMS = [
     ('A 1111.1111:', 'A!0000001111!0000001111!0000000000!0000000000!'),
-    ('A 1111.1111:1111', 'A!0000001111!0000001111!0000000000!0000000000!0000001111')
+    ('A 1111.1111:1111',
+     'A!0000001111!0000001111!0000000000!0000000000!0000001111')
 ]
 
 
@@ -27,5 +28,5 @@ def test_lc_cn_normalization(cn, expected):
 
 
 @pytest.mark.parametrize('cn, expected', SUDOC_NORMALIZATION_TEST_PARAMS)
-def test_lc_cn_normalization(cn, expected):
+def test_sudoc_cn_normalization(cn, expected):
     assert h.NormalizedCallNumber(cn, 'sudoc').normalize() == expected
